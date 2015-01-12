78a3ebd4ce9076456bc9648b9f924374bd450fa0
Magnus Palmér <magnus.palmer.work@gmail.com>
2015-01-12 14:39:23 +0100

    Forgot to update the test afer refactorings, also added unrollling the test with two files

--- src/test/groovy/MQSenderSpec.groovy
+++ src/test/groovy/MQSenderSpec.groovy
@@ -2,19 +2,36 @@ package org.eventfully.mqsi.event.collector.test
 
 import org.eventfully.mqsi.event.collector.component.RFHUtilResender
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class MQSenderSpec extends Specification {
 
+    @Unroll
     def "Store articleNumber #articleNumber"() {
 
         given: "A RFHutil file"
-        def testPayload = new File('src/test/resources/data/InputOrderWithRFH2.rfh')
+        def testPayload = new File("src/test/resources/data", inputFile)
+        and: "A resender"
+        RFHUtilResender resender = new RFHUtilResender()
+        resender.with {
+            queue = "TEST.IN"
+            port = 2414
+            hostname = "localhost"
+            channel = "JMS"
+
+        }
 
         when: "The request is sent"
-        boolean result = RFHUtilResender.resend(testPayload.getBytes())
+        boolean result = resender.resend(testPayload.getBytes())
 
         then: "A reply is received"
         result
+
+        where:
+        inputFile | _
+        'InputOrder.rfh' | _
+        'InputOrderWithRFH2.rfh' | _
+
     }
 
 }
