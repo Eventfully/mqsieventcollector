fb8e36eca720d3d5c310a4c7a58a4561e7a5e449
Magnus Palmér <magnus.palmer.work@gmail.com>
2015-01-13 14:10:11 +0100

    also include time HHss in the filename

--- src/main/groovy/org/eventfully/mqsi/event/collector/route/EventConsumerRoute.groovy
+++ src/main/groovy/org/eventfully/mqsi/event/collector/route/EventConsumerRoute.groovy
@@ -6,6 +6,8 @@ import org.apache.camel.Message
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
 
+import java.text.SimpleDateFormat
+
 @Component
 class EventConsumerRoute extends RouteBuilder {
 
@@ -13,7 +15,7 @@ class EventConsumerRoute extends RouteBuilder {
     void configure() throws Exception {
 
         from("{{eventRoute.from}}").routeId("{{eventRoute.id}}")
-                .log( LoggingLevel.INFO, "Event received")
+                .log(LoggingLevel.INFO, "Event received")
                 .convertBodyTo(String.class, "UTF-8")
                 .process { Exchange ex ->
 
@@ -27,7 +29,7 @@ class EventConsumerRoute extends RouteBuilder {
             String uniqueFlowName = event."wmb:eventPointData"."wmb:messageFlowData"."wmb:messageFlow"."@wmb:uniqueFlowName".text()
             String eventName = event."wmb:eventPointData"."wmb:eventData"."wmb:eventIdentity"."@wmb:eventName".text()
             String creationTime = event."wmb:eventPointData"."wmb:eventData"."wmb:eventSequence"."@wmb:creationTime".text()
-            String creationDate = creationTime?.substring(0, 9).replace('-', '')
+            String creationDate = creationTime?.substring(0, 17).replaceAll('[-:T]', '')
 
             String fileName = "/${uniqueFlowName.replace('.', '/')}/${eventName}-${creationDate}-${ex.exchangeId}.rfh"
             message.setHeader(Exchange.FILE_NAME, fileName)
