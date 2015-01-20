88a44eb200705604612b96e9a9e0f2829af543b7
Magnus Palmér <magnus.palmer.work@gmail.com>
2015-01-20 15:52:47 +0100

    cleanup

--- src/test/resources/resendconfig.groovy
+++
@@ -1,8 +0,0 @@
-File eventXml= new File('resend.xml')
-
-def configuration = new XmlParser(false, false).parse(eventXml)
-
-def currentFlow = "TotalPurchaseOrderFlow"
-def currentEventSrc = "InputOrder.transaction.Start"
-
-configuration."${currentFlow}".find { it.@eventSrc == currentEventSrc }.@resendQueue
